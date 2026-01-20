e87d6b8849b51f823d862ab2cf8a14a9e4663f2d
slashfmk <slashcs7@hotmail.com>
2026-01-20 05:01:20 -0500

    @SerialName("poster_path")     val movieImg: String has been serialized

--- app/src/main/java/com/oxygenik/moviz/retrofit/Movie.kt
+++ app/src/main/java/com/oxygenik/moviz/retrofit/Movie.kt
@@ -16,7 +16,7 @@ data class Movie(
     val popularity: Double,
 
     @SerialName("poster_path")
-    val posterPath: String,
+    val movieImg: String,
 
     val release_date: String,
     val title: String,
