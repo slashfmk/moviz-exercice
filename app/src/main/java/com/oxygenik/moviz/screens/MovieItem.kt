7a1263f6da8ae237764c44c44b225fda5f0c1a60
slashfmk <slashcs7@hotmail.com>
2026-01-21 07:50:22 -0500

    RoundedCornerShape added to the MovieItem.kt images clip(RoundedCornerShape(20.dp))

--- app/src/main/java/com/oxygenik/moviz/screens/MovieItem.kt
+++ app/src/main/java/com/oxygenik/moviz/screens/MovieItem.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
@@ -34,6 +36,7 @@ fun MovieItem(movie: Movie) {
             contentDescription = "Movie desc",
             contentScale = ContentScale.Crop,
             modifier = Modifier
+                .clip(RoundedCornerShape(20.dp))
                 .width(200.dp)
                 .height(250.dp)
         )
